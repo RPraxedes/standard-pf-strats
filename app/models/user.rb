4ba0d9d2606f79fe6884c0c5d2308e273a823ac7
Richard <richard@clinicdev.com>
2022-06-17 13:06:27 +0800

    Add administrate dashboard and login function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
   attr_accessor :password
 
-	# check to see if a supplied user password matches the hashed version in the database
-	def authenticate(password)
-		if BCrypt::Password.new(self.password_digest) == password
-			true
-		else
-			false
-		end
-	end
+  # check to see if a supplied user password matches the hashed version in the database
+  def authenticate(password)
+    if BCrypt::Password.new(self.password_digest) == password
+      true
+    else
+      false
+    end
+  end
 
-	# Create a bcrypt hashed version of a plain text password
-	def hash_password
-		if password.present?
-			return self.password_digest = BCrypt::Password.create(password)
-		end
-	end
+  # Create a bcrypt hashed version of a plain text password
+  def hash_password
+    if password.present?
+      return self.password_digest = BCrypt::Password.create(password)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :data_centers
+      resources :raids
+      resources :regions
+      resources :strategies
+
+      root to: "data_centers#index"
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/login" => "login#login_form"
+  post "login" => "login#check_login"
 end
